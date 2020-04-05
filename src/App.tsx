2662292432fc6f8354f9ed2e2efa092c4bee4ce7
dev-yakuza <dev.yakuza@gmail.com>
2020-04-05 15:27:01 +0900

    add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import Styled from 'styled-components/native';
+
+const Container = Styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Label = Styled.Text``;
 
 const App = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello world!</Text>
-    </View>
+    <Container>
+      <Label>Hello world!</Label>
+    </Container>
   );
 };
 
