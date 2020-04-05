774688dc852405cca8d6f377240793f3d8f73fb3
dev-yakuza <dev.yakuza@gmail.com>
2020-04-05 15:18:32 +0900

    add prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-const App = () => {
+const App = (): JSX.Element => {
   return (
     <View>
       <Text>Hello world!</Text>
